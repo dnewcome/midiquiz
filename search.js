006bb14044dcb44de63348e6df671f985fd4798b
hermanoscano <stevenscano@gmail.com>
2014-07-17 22:31:12 -0700

    added all the rest of the chords and a few intervals

--- search.js
+++ search.js
@@ -7,50 +7,442 @@ var trie = {
 	chord: null,
 	children: [
 		{
-			value: 3,
-			chord: null,
+			value: 2,
+			chord: ' (Interval: Major 2nd)',
 			children: [
+				{
+					value: 7,
+					chord: 'sus2',
+					children: []
+				}	
+					 ]
+		},
+		{
+			value: 3,
+			chord: ' (Interval: minor third)',
+			children: [
+				
+				{
+					value: 6,
+					chord: ' diminished fifth (augmented fourth)',
+					children: [
+							{
+								value: 9,
+								chord: 'dim7',
+								children: [
+									{
+
+										value: 13,
+										chord: ' Diminished Minor 9',
+										children: [
+											{
+												value: 16,
+												chord: ' Diminished 11',
+												children: []
+											}
+												  ]
+									},
+									{
+
+										value: 14,
+										chord: ' Diminished 9',
+										children: []
+									}
+										  ]
+							},
+							{
+								value: 10,
+								chord: 'm7b5',
+								children: [
+									
+										{
+
+										value: 13,
+										chord: ' Half Dim Minor 9',
+										children: []
+									},
+									{
+
+										value: 14,
+										chord: ' Half Diminished 9',
+										children: [
+											{
+												value: 17,
+												chord: ' Half Diminished 11',
+												children: [
+													{
+														value: 21,
+														chord: ' half Diminished 13',
+														children: []
+													}
+														  ]
+											}
+												 ]
+									}
+								]
+								}
+					]
+				},
 				{
 					value: 7,
 					chord: 'min',
 					children: [
 						{
+							value: 9,
+							chord: 'm6 (add 6)',
+							children: [
+								{
+									value: 10,
+									chord: 'm7add13',
+									children: []
+								},
+								{
+									value: 11,
+									chord: 'mMaj7add13',
+									children: []
+								},
+								{
+									value: 14,
+									chord: 'm6add9',
+									children: []
+								}
+									  ]
+						},
+						{
 							value: 10,
 							chord: 'm7',
+							children: [
+								{
+									value: 13,
+									chord: 'm7b9',
+									children: []
+								},
+								{
+									value: 14,
+									chord: 'm9',
+									children: [
+										{
+											value: 17,
+											chord: ' m11',
+											children: [
+												{
+													value: 21,
+													chord: 'm13',
+													children: []
+												}
+													  ]
+										}
+											  ]
+								},
+								{
+									value: 17,
+									chord: 'm7add11',
+									children: []
+								}
+							]
+						},
+						{
+							value: 11,
+							chord: 'm(Maj7)',
+							children: [
+								{
+									value: 14,
+									chord: ' minor/Major 9',
+									children: [
+										{
+											value: 17,
+											chord: ' minor/Major 11',
+											children: [
+												{
+													value: 21,
+													chord: ' minor/Major 13',
+													children: []
+												}
+													  ]
+										}
+											  ]
+								},
+								{
+									value: 17,
+									chord: 'mMaj7add11',
+									children: []
+								}
+							]
+						},
+
+						{
+							value: 14,
+							chord: 'madd9',
+							children: []
+						},
+						{
+							value: 17,
+							chord: 'madd11',
 							children: []
 						}
 					]
 				},
-				{
-					value: 6,
+				
+					{
+					value: 8,
 					chord: 'dim',
-					children: []
+					children: [
+						{
+								value: 10,
+								chord: 'm7#5',
+								children: []
+								}
+					]
 				}
 			]
 		},
 		{
 			value: 4,
-			chord: null,
+			chord: '(Interval: Major 3rd)',
 			children: [
+				
 				{
-					value: 7,
-					chord: 'maj',
+					value: 6,
+					chord: 'add#11',
 					children: [
 						{
-							value: 11,
-							chord: 'maj7',
+							value: 10,
+							chord: '7b5',
 							children: []
+
 						}
+							  ]
+				},
+				{
+					value: 7,
+					chord: 'Maj',
+					children: [
+						{
+							value: 9,
+							chord: 'Maj6 (add6)',
+							children: [
+								{
+									value: 10,
+									chord: '7add13',
+									children: []
+								},
+								{
+									value: 11,
+									chord: 'Maj7add13',
+									children: []
+								},
+								{
+									value: 14,
+									chord: '6add9',
+									children: []
+								}
+									  ]
+							  
+						},
+							
+						{
+							value: 10,
+							chord: '7',
+							children: [
+								
+								{
+
+									value: 13,
+									chord: ' Dominant 7b9',
+									children: []
+								},
+								{
+
+									value: 14,
+									chord: '9 (Dominant 9)',
+									children: [
+										{
+											value: 17,
+											chord: ' Dominant 11',
+											children: [
+												{
+													value: 21,
+													chord: ' Dominant 13',
+													children: []
+												}
+												   	  ]
+										},
+										{
+											value: 18,
+											chord: ' Dominant 9#11',
+											children: []
+										},
+										{
+											value: 20,
+											chord: ' Dominant 9b13',
+											children: []
+										}
+											]
+								},
+
+								{
+									value: 15,
+									chord: ' Dominant 7#9',
+									children: []
+								},
+								{
+									value: 17,
+									chord: '7add11',
+									children: []
+								}
+							]
+						},
+						{
+							value: 11,
+							chord: 'Maj7',
+							children: [
+								{
+									value: 14,
+									chord: 'Maj9',
+									children: [
+										{
+											value: 17,
+											chord: 'Maj11',
+											children: [
+												{
+													value: 21,
+													chord: 'Maj13',
+													children: []
+												}
+													  ]
+										}	
+											  ]
+								},
+								{
+									value: 17,
+									chord: 'Maj7add11',
+									children: []
+								}
+								
+									  ]
+
+						},
+						{
+							value: 14,
+							chord: 'add9',
+							children: []
+						},
+						{
+							value: 17,
+							chord: 'add11',
+							children: []
+						}	
 					]
 				},
 				{
 					value: 8,
 					chord: 'aug',
-					children: []
+					children: [
+						{
+							value: 10,
+							chord: 'aug7(7#5)',
+							children: [
+
+								{
+									value: 13,
+									chord: ' Dominant 7#5b9',
+									children: []
+								},
+								{
+
+									value: 14,
+									chord: ' Augmented 9',
+									children: [
+										{
+												value: 17,
+												chord: ' Augmented 11',
+												children: [
+													{
+															value: 21,
+															chord: ' Augmented 13',
+															children: []
+													}
+														  ]
+										}
+											  ]
+								}
+							]
+
+						},	
+						{
+							value: 11,
+							chord: '+M7 (Augmented Major 7))',
+							children: [
+								{
+									value: 14,
+									chord: ' Augmented Major 9',
+									children: [
+										{
+												value: 17,
+												chord: ' Augmented Major 11',
+												children: [
+													{
+															value: 21,
+															chord: ' Augmented Major 13',
+															children: []
+													}
+														  ]
+										}
+											  ]
+								}
+							]
+						}	
+					]
 				}
 			]
+		},
+		{
+			value: 5,
+			chord: null,
+			children: [
+				{
+					value: 7,
+					chord: 'sus4',
+					children: [
+						{
+							value: 9,
+							chord: '6sus4',
+							children: []
+						},
+						{
+							value: 10,
+							chord: '7sus4',
+							children: [
+								{
+									value: 13,
+									chord: '7sus4b9',
+									children: []
+								},
+								{
+									value: 14,
+									chord: '9sus4',
+									children: []
+								}
+									  ]
+						},
+						{
+							value: 11,
+							chord: 'Maj7sus4',
+							children: [
+								{
+									value: 14,
+									chord: 'Maj9sus4',
+									children: []
+								}
+									  ]
+						}
+							  ]
+				}	
+					 ]
 		}
+
 	]
+
 }; 
 
 /**
