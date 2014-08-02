00a0ff7f6bd0e9d071685181190b41b5493c9d54
Dan Newcome <dan@dnuke.com>
2014-08-02 14:06:34 -0700

    putting chord degree in here

--- chord-quiz.js
+++ chord-quiz.js
@@ -5,6 +5,7 @@ function Quiz(root, dom) {
 	this.step = 0;
 	this.root = root;
 	this.chords = ['maj', 'min', '7', 'maj', '7', 'min', 'dim'];
+	this.degree = ['I', 'ii', 'III7', 'IV', 'V7', 'vi', 'VII'];
 	this.notes = [0, 2, 4, 5, 7, 9, 11];
 }
 
@@ -12,10 +13,11 @@ Quiz.prototype.next = function() {
 	var idx = Math.floor(Math.random()*(this.chords.length));
 	var ret = {
 		type: this.chords[idx],
-		root: this.notes[idx]
+		root: this.notes[idx],
+		degree: this.degree[idx]
 	};
 
-	this.dom.innerHTML = numberToNote(ret.root).note + ' ' + ret.type;
+	this.dom.innerHTML = numberToNote(ret.root).note + ' ' + ret.type + ' (' + ret.degree + ')';
 	return ret;
 }
 
