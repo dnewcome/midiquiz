b9359f94d6e24cb6abc59db6cce9cd231bfb7121
hermanoscano <stevenscano@gmail.com>
2014-08-02 13:01:19 -0700

    added midi basic quiz test outline procedure

--- tests.js
+++ tests.js
@@ -37,3 +37,33 @@ QUnit.test( 'no chord found', function( assert ) {
 	var chord = search([0, 3, 7, 8, 9]);	
 	assert.equal(chord, null, 'failed' );
 });
+
+/*
+1.	User select 1/12 harmonic scales 
+C major / A minor
+C#(Db) major / A#(Bb) minor
+D major / B minor
+D# (Eb) major / B# (C) minor
+E major / C#(Db) minor
+F major / D minor
+F#(Gb) major /  D#(Eb) minor
+G major / E Minor
+Ab (G#)  major / F minor
+A major / F#(Gb)  minor
+Bb (A#) major / G minor
+B major  / G#(Ab) minor
+2.	Pick Midi test (i.e. Basic, Advanced, Master, Custom)
+
+3.	Pick number of Quiz Questions
+choose 25,50,75,100
+
+4.	Generate Test
+5.	Display harmonic degree (
+	I
+•	IIm
+•	III7
+•	IV
+•	V
+•	VIm
+•	VII°
+*/
